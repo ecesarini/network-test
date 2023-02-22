9297c1c6c3fa7c52e9993f465b2519d3d2bbbec3
ecesarini <73652902+ecesarini@users.noreply.github.com>
2023-02-22 23:45:54 +0100

    ref doc env

---
+++ vm2/workdir/conf_sw.sh
@@ -0,0 +1,10 @@
+echo "Getting env variables"
+source var_cont.sh 
+source var_node.sh
+
+#------------- Communication with between VM --------------#
+echo "Setting the rout on the node to reach the net ns"
+ip route add $TO_BRIDGE_SUBNET via $TO_NODE_IP_SW dev enp0s8
+
+echo "Enables IP forwarding on the node (-w stands for write)"
+sysctl -w net.ipv4.ip_forward=1
---
+++ vm2/workdir/testing_sw.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+source var_cont.sh
+source var_node.sh
+#---Testing---#
+
+#Ping adaptor attached to NS1
+ip netns exec $NS1 ping -c 3 172.16.1.2
+
+#Ping the bridge
+ip netns exec $NS1 ping -c 3 172.16.1.1
+
+#Ping adaptor of the second container
+ip netns exec $NS1 ping -c 3 172.16.1.3
+
+#Ping the other server (VM1)
+ip netns exec $NS1 ping -c 3 $TO_NODE_IP_SW
+
+#Ping the bridge on the other server
+ip netns exec $NS1 ping -c 3 172.16.0.1
+
+#Ping the first container on VM1
+ip netns exec $NS1 ping -c 3 172.16.0.2
+
+#Ping the second container on VM1
+ip netns exec $NS1 ping -c 3 172.16.0.3
