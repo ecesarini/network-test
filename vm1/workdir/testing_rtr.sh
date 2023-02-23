f0db0f2fb49d643f06e50de628afaa92af2627ec
ecesarini <73652902+ecesarini@users.noreply.github.com>
2023-02-23 21:20:16 +0100

    update vm1 testing_rtr.sh

--- vm1/workdir/testing_rtr.sh
+++ vm1/workdir/testing_rtr.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-echo "Getting env variables"
-source var_cont.sh
-source var_node.sh
-
-#------------- Communication between VM --------------#
-echo "Setting the rout on the node to reach the net ns"
-ip route add $TO_BRIDGE_SUBNET via $TO_NODE_IP_SW dev enp0s8
-
-echo "Enables IP forwarding on the node (-w stands for write)"
-sysctl -w net.ipv4.ip_forward=1
-root@ubuntu-vm-01:~/test-k8s-net# cat testing_rtr.sh
-#!/bin/bash
 source var_cont.sh
 #source var_node.sh
 #Check routes in container1
